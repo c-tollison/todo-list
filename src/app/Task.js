7d5828d66d5320fe84a8b1ecfd22f6841bc9be72
c-tollison <tollisoncarson@gmail.com>
2023-09-12 00:29:37 -0400

    Sliding side bar, Simple nav

--- src/app/Task.js
+++ src/app/Task.js
@@ -12,16 +12,14 @@ export default class Task {
         task.classList.add(
             ...[
                 "container",
-                "px-4",
+                "px-2",
                 "py-4",
                 "w-80",
                 "h-30",
                 "rounded",
                 "shadow",
                 "flex",
-                "border",
-                "border-gray-400",
-                "bg-white",
+                "bg-[#242629]",
                 "gap-6",
                 "hover:border-gray-700",
             ]
@@ -35,12 +33,12 @@ export default class Task {
         let checkButton = document.createElement("button");
         checkButton.classList.add(
             ...[
-                "w-6",
-                "h-6",
+                "w-4",
+                "h-4",
                 "border",
-                "border-gray-400",
+                "border-[#7f5af0]",
                 "rounded-full",
-                "hover:border-gray-700",
+                "hover:border-[#94a1b2]",
             ]
         );
         checkButton.addEventListener("click", (event) => {
@@ -54,15 +52,15 @@ export default class Task {
 
         let name = document.createElement("p");
         name.textContent = this.name;
-        name.classList.add(...["text-gray-900", "font-bold", "text-lg"]);
+        name.classList.add(...["text-[#fffffe]", "font-bold", "text-lg"]);
 
         let description = document.createElement("p");
         description.textContent = this.description;
-        description.classList.add(...["text-gray-700", "text-sm"]);
+        description.classList.add(...["text-[#94a1b2]", "text-sm"]);
 
         let priority = document.createElement("p");
         priority.textContent = "P: " + this.priority;
-        priority.classList.add(...["text-gray-600", "text-sm"]);
+        priority.classList.add(...["text-[#94a1b2]", "text-sm"]);
 
         rightSide.append(name, description, priority);
 
--- src/app/index.js
+++ src/app/index.js
@@ -4,10 +4,14 @@ import Task from "./Task";
 
 let taskContainer = document.getElementById("taskContainer");
 let taskModal = document.getElementById("taskModal");
+taskModal.addEventListener("click", () => {
+    taskModal.classList.add("hidden");
+    taskForm.reset();
+});
 
 let taskForm = document.getElementById("taskForm");
-taskForm.addEventListener("submit", (event) => {
-    event.preventDefault();
+taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
     taskModal.classList.add("hidden");
     const formValues = {
         name: document.getElementById("taskName").value,
@@ -21,7 +25,31 @@ taskForm.addEventListener("submit", (event) => {
     taskForm.reset();
 });
 
+let modalCloseButton = document.getElementById("modalCloseButton");
+modalCloseButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    taskModal.classList.add("hidden");
+    taskForm.reset();
+});
+
 let addTaskButton = document.getElementById("addTaskButton");
 addTaskButton.addEventListener("click", () => {
     taskModal.classList.remove("hidden");
 });
+
+let sideBar = document.getElementById("sideBar");
+let toggleSideBar = document.getElementById("toggleSideBar");
+toggleSideBar.addEventListener("click", () => {
+    sideBar.classList.toggle("-left-44");
+    sideBar.classList.toggle("left-0");
+});
+
+const formValues = {
+    name: "exampleTitle",
+    description: "example Description",
+    dueDate: "09/14/2000",
+    priority: 2,
+};
+
+let newTask = new Task(formValues);
+newTask.renderTask(taskContainer);
